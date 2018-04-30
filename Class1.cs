b2a2b16f64a6c843106c2bd77161fb55d4303c60
Andrey Vernigora <andrey_vernigora@epam.com>
2018-04-30 12:23:52 +0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Management.Automation;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Linq.Dynamic.Core;
+
+namespace dinLinqTestModule
+{
+    [Cmdlet("Get", "Test")]
+    public class GetTestCmdlet : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+
+            var values = new[] { -1, 2, 3, 4, 5, 6, 10 }.AsQueryable();
+
+            var result = values.Where("it = 2").ToArray();
+
+            foreach (var r in result)
+                WriteObject(r);
+        }
+    }
+}
