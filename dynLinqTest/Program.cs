501332ae628fa64c01671f3d5c04f75b371ee20f
Andrey Vernigora <andrey_vernigora@epam.com>
2018-04-30 12:29:37 +0300

    another attempt

---
+++ dynLinqTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Linq.Dynamic.Core;
+
+namespace dynLinqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var values = new[] { -1, 2, 3, 4, 5, 6, 10 }.AsQueryable();
+
+            var result = values.Where("it = 2").ToArray();
+
+            foreach (var r in result)
+                Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
